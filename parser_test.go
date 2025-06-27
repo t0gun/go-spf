94a36ccce9f92be842ca35bb8594d72e5f80cb66
tobiloba ogundiyan <apprentice_py@icloud.com>
2025-06-27 13:15:07 -0400

    added ip6 parser

--- parser_test.go
+++ parser_test.go
@@ -18,6 +18,11 @@ func mechip4(q Qualifier, cidr string) Mechanism {
 	return Mechanism{Qual: q, Kind: "ip4", Net: n}
 }
 
+func mechip6(q Qualifier, cidr string) Mechanism {
+	_, n, _ := net.ParseCIDR(cidr)
+	return Mechanism{Qual: q, Kind: "ip6", Net: n}
+}
+
 func TestParse(t *testing.T) {
 	cases := []struct {
 		name    string
@@ -37,14 +42,31 @@ func TestParse(t *testing.T) {
 			want: []Mechanism{mech(QPlus, "all")}},
 
 		{
-			name:    "bad cidr",
+			name:    "bad cidr ip4",
 			spf:     "v=spf1 ip4:203.0.113.0/99 -all",
 			wantErr: true,
 		},
 		{
-			name: "ipv with no mask then ~all",
+			name: "ip4 with no mask then ~all",
 			spf:  "v=spf1 +ip4:203.0.113.23 ~all",
 			want: []Mechanism{mechip4(QPlus, "203.0.113.23/32"), mech(QTilde, "all")},
+		},
+
+		{
+			name: "ip6 and ip4 then -all",
+			spf:  "v=spf1 ip6:2001:db8::/32 ip4:203.0.113.0/24 -all",
+			want: []Mechanism{mechip6(QPlus, "2001:db8::/32"), mechip4(QPlus, "203.0.113.0/24"), mech(QMinus, "all")},
+		},
+
+		{
+			name: "implicit /128 host",
+			spf:  "v=spf1 ip6:2001:db8::1 -all",
+			want: []Mechanism{mechip6(QPlus, "2001:db8::1/128"), mech(QMinus, "all")},
+		},
+		{
+			name:    "bad ipv6 cidr",
+			spf:     "v=spf1 ip6:2001:db8::/200 -all",
+			wantErr: true,
 		},
 	}
 
@@ -61,58 +83,4 @@ func TestParse(t *testing.T) {
 		})
 	}
 
-}
-
-func TestTokenizer(t *testing.T) {
-	cases := []struct {
-		name    string
-		spf     string
-		want    []string
-		wantErr bool
-	}{
-		{
-			name: "minimal -all",
-			spf:  "v=spf1 -all",
-			want: []string{"-all"},
-		},
-		{
-			name: "uppercase prefix, extra spaces",
-			spf:  "  V=SPF1    ip4:203.0.113.0/24   -all ",
-			want: []string{"ip4:203.0.113.0/24", "-all"},
-		},
-		{
-			name: "tabs and newlines",
-			spf:  "v=spf1\tip4:198.51.100.0/24\r\n-all",
-			want: []string{"ip4:198.51.100.0/24", "-all"},
-		},
-		{
-			name:    "no prefix",
-			spf:     "-all",
-			wantErr: true,
-		},
-		{
-			name:    "prefix but no terms",
-			spf:     "v=spf1   ",
-			wantErr: true,
-		},
-		{
-			name: "multiple tokens",
-			spf:  "v=spf1 a mx ptr ~all",
-			want: []string{"a", "mx", "ptr", "~all"},
-		},
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			got, err := tokenizer(tc.spf)
-
-			if tc.wantErr {
-				require.Error(t, err)
-				return
-			}
-
-			require.NoError(t, err)
-			assert.Equal(t, tc.want, got)
-		})
-	}
 }
